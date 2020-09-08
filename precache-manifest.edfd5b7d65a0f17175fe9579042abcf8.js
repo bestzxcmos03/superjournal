ec9cbddf2274272b24cb0dec68b4721d5c3e3a4a
moneykeeper <bananasontop@gmail.com>
2020-09-08 16:46:20 +0700

    Updates

---
+++ precache-manifest.edfd5b7d65a0f17175fe9579042abcf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761d7208b7098e15d26e6ad4fc477cee",
+    "url": "/superjournal/index.html"
+  },
+  {
+    "revision": "be844835fd2ae8baae7e",
+    "url": "/superjournal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5547e260d968c1052da6",
+    "url": "/superjournal/static/js/2.fd488a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superjournal/static/js/2.fd488a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be844835fd2ae8baae7e",
+    "url": "/superjournal/static/js/main.04f3e0b6.chunk.js"
+  },
+  {
+    "revision": "551c03f2046062bb20e8",
+    "url": "/superjournal/static/js/runtime-main.499f1035.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/superjournal/static/media/logo.5d5d9eef.svg"
+  }
+]);
